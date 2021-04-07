32b9c934d362295c90287ac2ed296fc5fac7ffd5
Valentin <valentin.skliarov@gmail.com>
2021-04-07 21:12:47 +0300

    Ver 0.0002

--- Source.cpp
+++ Source.cpp
@@ -26,35 +26,35 @@ void main()
 {
 	setlocale(LC_ALL, "");
 	cout << "\t    Морской бой\n";
-	/*int menu;
+	int menu;
 	cout << "Выберите с кем будете играть:\n1 - компьютер\n2 - другой игрок" << endl;
 	cin >> menu;
-	switch (menu)
-		case 1: 
-			cout << "Извините, но функция пока в разработке =)" << endl;
-			menu = 2; break;
-		case 2: 
-			srand(time(NULL));
-			int temp = 0;
-			cout << "Определите кто игрок №1, а кто №2 и нажмите Enter" << endl;
-			system("CLS");
-			system("pause");
-			for (int i = 0; i < 2; i++)
+	switch (menu) {
+	case 1:
+		cout << "Извините, но функция пока в разработке =)" << endl;
+	case 2:
+		srand(time(NULL));
+		int temp = 0;
+		cout << "Определите кто игрок №1, а кто №2 и нажмите Enter" << endl;
+		system("CLS");
+		system("pause");
+		for (int i = 0; i < 2; i++)
+		{
+			temp = rand() % 2;
+			if (temp == false)
 			{
-				temp = rand() % 2;
-				if (temp == false)
-				{
-					cout << "Первым ходит игрок №1";
-					system("pause");
-					system("CLS");
-				}
-				else
-				{
-					cout << "Первым ходит игрок №2";
-					system("pause");
-					system("CLS");
-				}
-			}; break;*/
+				cout << "Первым ходит игрок №1";
+				system("pause");
+				system("CLS");
+			}
+			else
+			{
+				cout << "Первым ходит игрок №2";
+				system("pause");
+				system("CLS");
+			}
+		}; break;
+	}
 	pole();
 
 }
